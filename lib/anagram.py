633879d4cfb0bb046b9ea486cc1c2b87297c0249
Michael Ouma <Oumamichael108@gmail.com>
2025-05-10 21:55:41 +0300

    Lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = sorted(word)
+
+    def match(self, candidates):
+        return [word for word in candidates if sorted(word) == self.sorted_word]    
+listen = Anagram("listen")
+silent = Anagram("silent")
+listen.match(['enlists', 'google', 'inlets', 'banana'])
+for word in listen.match(['enlists', 'google', 'inlets', 'banana']):
+    print(word)
+    sorted_word = sorted(word)
+    print(sorted_word)
